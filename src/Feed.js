8ac3e1d9e541fa5d4e53515fac033c655bed9562
ahujaprabhpreet <ahuja.pra@husky.neu.edu>
2020-12-25 03:33:00 -0500

    Feed UI added

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import './Feed.css'
+import InputOption from "./InputOption";
+import Post from "./Post";
+import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/Create";
 
 function Feed() {
     return (
         <div className = "feed">
-            <h1>This is Feed</h1>
+            <div className = "feed__inputContainer">
+                <div className = "feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type = "text" />
+                        <button type = "submit" >Send</button>
+                    </form>
+                </div>
+            
+
+                <div className = "feed__inputOptions">
+                    <InputOption Icon = {ImageIcon} title = 'Photo' color = '#70B5F9' />
+                    <InputOption Icon = {SubscriptionsIcon} title = 'Video' color = '#E7A33E' />
+                    <InputOption Icon = {EventNoteIcon} title = 'Event' color = '#C0CBCD' />
+                    <InputOption Icon = {CalendarViewDayIcon} title = 'Write Article' color = '#7FC15E' />
+                </div>
+            </div>
+
+            {/* posts */}
+            <Post name ='Pappy' description = 'this is a test' message = 'Wow! It worked'/>
         </div>
     )
 }
